39e2eb7183f2f8ed210ebcfa1946e6a6ada4b9f8
Myodong <gyehd1230@naver.com>
2022-08-11 17:08:37 +0900

    04_java_Array
    KH학원 수업 04

---
+++ 04_array/edu/kh/array/ex/ArrayExampleRun.java
@@ -0,0 +1,14 @@
+package edu.kh.array.ex;
+
+public class ArrayExampleRun {
+
+	public static void main(String[] args) {
+
+		ArrayExample arrayEX =new ArrayExample();	
+		
+		
+		arrayEX.ex1();
+		
+	}
+
+}
