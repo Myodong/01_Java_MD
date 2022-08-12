892fcd33944fc9a285b44b91807a0ebb437580c9
Myodong <gyehd1230@naver.com>
2022-08-12 17:34:38 +0900

    04_java_Array
    KH학원 수업 04

--- 04_array/edu/kh/array/ex/ArrayExampleRun.java
+++ 04_array/edu/kh/array/ex/ArrayExampleRun.java
@@ -7,7 +7,18 @@ public class ArrayExampleRun {
 		ArrayExample arrayEX =new ArrayExample();	
 		
 		
-		arrayEX.ex1();
+//		arrayEX.ex1();
+//		arrayEX.ex2();
+//		arrayEX.ex3();
+//		arrayEX.ex4();
+//		arrayEX.ex5();
+//		arrayEX.ex6();
+//		arrayEX.ex7();
+//		arrayEX.ex8();
+//		arrayEX.createlottoNumber();
+//		arrayEX.ex9();
+//		arrayEX.ex10();
+		arrayEX.ex11();
 		
 	}
 
