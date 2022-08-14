b1cdc6776fe69e8c790b77f389a1f624e0c19e9c
Myodong <gyehd1230@naver.com>
2022-08-14 17:34:45 +0900

    Update JavaTest.java

--- 00_java_Test/edu/kh/test/JavaTest.java
+++ 00_java_Test/edu/kh/test/JavaTest.java
@@ -1,21 +1,18 @@
 package edu.kh.test;
-
-//占쏙옙 占쏙옙 占쌍쇽옙( 占쏙옙占쏙옙占싹뤄옙(占쏙옙占쏙옙占쏙옙)占쏙옙 占쌔쇽옙占쏙옙占쏙옙 占십댐옙 占싸븝옙 )
-/* 占쏙옙占쏙옙 占쌍쇽옙  
-  占십울옙 占쏙옙占쏙옙 특占쏙옙 占싸븝옙 
+//한 줄 주석( 컴파일러(번역기)가 해석하지 않는 부분 )
+/* 범위 주석  
+  필요 없는 특정 부분 
    */
-//class : 占쌘뱄옙 占쌘드가 占쌜쇽옙占실댐옙 占쏙옙占쏙옙
+//class : 자바 코드가 작성되는 영역
 public class JavaTest {
-
-	//占쏙옙占쏙옙 占쌨쇽옙占쏙옙 : 占쌘뱄옙 占쏙옙占시몌옙占쏙옙占싱쇽옙(占쏙옙占싸그뤄옙)占쏙옙 占쏙옙占쏙옙占싹깍옙 占쏙옙占쌔쇽옙 占쌥듸옙占� 占십울옙占쏙옙 占싸븝옙 
+	//메인 메서드 : 자바 애플리케이션(프로그램)을 실행하기 위해서 반드시 필요한 부분 
 	public static void main(String[] args) {
 		//System.out.println("Hello World");
-		System.out.println("println()占쏙옙 占쏙옙 占쏙옙 占쏙옙占쏙옙都求占�.");
+		System.out.println("println()은 한 줄 출력입니다.");
 		System.out.println(100+200+300+400);
 		
-		System.out.println("占쌓쏙옙트");
+		System.out.println("테스트");
 	}
-
 }
 
-//print -占쏙옙占쏙옙構渼占� ln占쏙옙占쏙옙占쏙옙占�
+//print -출력하겠다 ln한줄출력
