aa3dd092966f5207bacb39b0d2661949430ae976
Myodong <gyehd1230@naver.com>
2022-08-23 16:31:04 +0900

    07_java_Inheritance
    KH학원 수업 07

--- 07_inheritance/edu/kh/inheritance/model/vo/Person.java
+++ 07_inheritance/edu/kh/inheritance/model/vo/Person.java
@@ -1,7 +1,11 @@
 package edu.kh.inheritance.model.vo;
 
-public class Person {
+public class Person extends Object{ // 자식이 고통적으로 가지고있는 필드/메서드 작성함( 추상화 )
 
+	// extends Object - 상속 구문이 작성되지 않으면
+	// 					컴파일러가 자동 으로 추가
+	
+	
 	private String name;
 	private int age;
 	
@@ -34,8 +38,11 @@ public class Person {
 	}
 	
 	
-	
-	
+	// 자기소개 기능
+	public void introduce() {
+		System.out.println("이름 : "+name);
+		System.out.println("나이 : "+age);
+	}
 	
 	
 	
--- 07_inheritance/edu/kh/inheritance/model/vo/Student.java
+++ 07_inheritance/edu/kh/inheritance/model/vo/Student.java
@@ -18,6 +18,35 @@ public class Student extends Person {
 	
 	
 	public Student() {
+	
+		// Person(); //부모생성자 상속X
+		super(); //super 생성자
+		// 부모의 생성자를 참조하기 위해서 사용하는 생성자
+		// 자식 생성자 내부 첫 번째 줄에만 작성 가능
+		
+		// -> 자식 객체 생성 시 내부에
+		//    부모 객체를 생성할 때 사용한다.
+		
+		// -> 자식 생성자 첫 번째줄에
+		// super()생성자 미작성 시 컴파일러가 자동 추가
+	}
+	
+	public  Student(String name ,int age, int grade, int classRoom) {
+
+		
+		// 상속받은 자식도 부모의 private 필드 직접 접근 불가
+//		this.name = name;
+//		this.age = age;
+		
+		//방법1. 부모의 getter/setter 이용
+//		setName(name);
+//		setAge(age);
+		
+		//방법2. super()생성자 지용
+		super(name, age);
+		
+		this.classRoom = classRoom;
+		this.grade = grade;
 	}
 
 
@@ -60,8 +89,26 @@ public class Student extends Person {
 		this.classRoom = classRoom;
 	}
 	
+	// Preson으로 부터 상속 받은 introduce() 메서드 오버라이딩(재정의)
 	
+	@Override 
+	public void introduce() {
+//		System.out.println("이름 : "+ super.getName());
+//		System.out.println("나이 : "+ super.getAge());
+		super.introduce();
+		// 오버라이딩 되지 않은 부모의 introduce()메서드를 호출
+		System.out.println("학년 : "+ this.getGrade());
+		System.out.println("반 : "+ this.getClassroom());
+		
+	}
 	
-	
+	/* Annotation(@) : 컴파일러 용 주서
+	 * -> 컴파일러에게 해당 코드가 무엇을 의미하는지
+	 * 		아니면 해당 코드를 수행하기 전에 무엇을 해야하는지 등을 알려줌
+	 * 
+	 *  @Override
+	 *  	1) 컴파일러에게 해당 메서드는 오버라이딩 되었음을 알려줌
+	 *  	2) 오버라딩이긴 가능한지, 잘못 작성되지 않았는지 검사
+	 */
 	
 }
