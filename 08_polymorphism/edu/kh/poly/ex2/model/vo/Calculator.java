df204452f2cb5834b2c6279d7775236481f55335
Myodong <gyehd1230@naver.com>
2022-08-24 16:26:37 +0900

    08_java_Polymorphism
    KH학원 수업 08

---
+++ 08_polymorphism/edu/kh/poly/ex2/model/vo/Calculator.java
@@ -0,0 +1,29 @@
+package edu.kh.poly.ex2.model.vo;
+
+// 계산기 인터페이스
+// -> 모든 계산기에 대한 공통 피드, 기능명을 제공하는 접점 용도
+public interface Calculator {
+
+	public static final double PI = 3.14;
+	static final int MAX_MUN = 1000000; //100만
+	final int MIN_MUN = -1000000; //-100만
+	int P_MUN = 333;
+	// --> 예약어가 어떻게 작성되든 public static final
+	
+	public abstract int plus(int num1, int num2); // 더하기
+	
+	abstract int minus(int num1, int num2); // 빼기
+	
+	abstract int multiple(int num1, int num2); // 곱하기
+	
+	abstract double divide(int num1, int num2); // 나누기
+
+	abstract double areaOfCircle(double r); // 원 넓이
+								// 반지름
+
+	double pow(double a, int b); // a의 b제곱
+
+
+
+
+}
