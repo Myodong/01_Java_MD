339ebd1d9b86dd25311ab528fd6ccb41795ab6c7
Myodong <gyehd1230@naver.com>
2022-08-25 16:24:09 +0900

    10_java_Collection
    KH학원 수업 10

--- 10_collection/edu/kh/collection/model/service/SetService.java
+++ 10_collection/edu/kh/collection/model/service/SetService.java
@@ -3,6 +3,9 @@ package edu.kh.collection.model.service;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
+
+import edu.kh.collection.model.vo.Student;
 
 public class SetService {
 
@@ -75,6 +78,124 @@ public class SetService {
 			System.out.println(temp);
 		}
 	}
+	
+	
+	public void ex2() {
+		
+		/* Set에 저장된 객체가 중복임을 판단하는 방법
+		 * 
+		 * - 필드에 저장된 값이 모두 일치하면 중복으로 판단
+		 * -> 중복 판단에 사용하는 메서드
+		 * 		Object.equals() 메서드의 오버라이딩된 메서드
+		 * 		-> Student 오버라이딩
+		 * 
+		 * 
+		 * Hash라는 단어가 붙은 컬렉션이 중복을 판단하는 방법
+		 * -> Object.hashCode() 오버라이딩
+		 * 
+		 * Hash 함수 : 입력된 단어를 지정된 길이의 무작위 문자열로 변환하는 함수
+		 * 
+		 * hashCode() : 객체에 저장된 필드를 이용해서 만들어진 정수를 반환
+		 * 
+		 * HashSet -> equals(),hashCode() 둘 다 오버라이딩 되어 있어야 함
+		 * 
+		 * -> equals() 또는 hashCode()를 오버라이딩 하면
+		 * 		나머지 하나도 반드시 오버라이딩을 진행해라
+		 * 
+		 * 
+		 * 
+		 */
+		
+		Set<Student> set = new HashSet<Student>();
+		
+		set.add(new Student("이충재",28,"영등포",'M',60));
+		set.add(new Student("노성찬",29,"경기도 읍네",'M',100));
+		set.add(new Student("이충재",28,"영등포",'M',60));
+		
+		
+		Student s1 = new Student("노성찬",29,"경기도 읍네",'M',100);
+		Student s2 = new Student("노성찬",29,"경기도 읍네",'M',100);
+		
+		
+		System.out.println(s1.equals(s2));
+		
+		
+		
+		
+		
+		for (Student s : set) {
+			System.out.println(s);
+		}
+
+	}
+	
+	public void ex3() {
+		
+		
+		
+		// TreeSet : 오름차순으로 정렬되고 중복이 제거되는 Set
+		
+		
+		// Wrapper Class
+		
+		// * 컬렉션은 객체만 저장 가능(기본자료형 x)
+		//	-> 컬렉션에 기본 자료형도 저장할 수 있는 방법
+		//	--> 기본 자료형을 포장하여 객체로 만듦.
+		
+		// boolean -> Boolean
+		// byte    -> Byte
+		// short   -> Short
+		// int	   -> Integer
+		// long	   -> Long
+		// float   -> Float
+		// double  -> Double
+		// char	   -> Character
+		
+		// Wrapper 클래스는 기본 자료형 값 + 추가 필드,메서드 제공
+		
+		System.out.println(Integer.MAX_VALUE);
+		
+		int num = Integer.parseInt("123");
+		double dNum = Double.parseDouble("123.123");
+		// 문자열 이지만 내용이 모두 숫자인 문자열이 존재
+		// -> Wrapper 클래스의 parseXXX()를 이용하면 숫자로 변경 가능
+		
+		// parsing : 데이터의 형식 자체를 바꾸는 것
+		
+		System.out.println(num + 100); // 223
+		System.out.println(dNum + 100); // 223.123
+		
+		// Auto Boxing(자동 포장)
+		Integer i1 =10; // Integer = int
+						// Integer = new Integer(int) // 컴파일러가 바꿔줌
+		
+		// Auto UnBoxing (자동 포장 해제)
+		int i2 = i1; // int = Integer
+		
+		System.out.println(i1.intValue()  );
+		
+		
+		
+		Set<Integer> lotto = new TreeSet<Integer>();
+		
+		// Integer -> int의 Wrapper Class
+		
+		while (lotto.size()<6) {
+		
+			int ran = (int)(Math.random()*45+1); // 1~45난수
+			
+			lotto.add(ran);
+		}
+		System.out.println(lotto); // 로또 번호 출력
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
